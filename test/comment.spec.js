5677e3ddfab1dc13a6a5f54a9139c2964a59e8a6
elcarim5efil <elcarim5efil@gmail.com>
2018-11-21 11:18:21 +0800

    test: add test for comment

---
+++ test/comment.spec.js
@@ -0,0 +1,15 @@
+const { assert } = require('./helper');
+
+describe('comment', function() {
+  it('should bew reserved', () => {
+    assert(
+      `<div><!-- i am comment --></div>`,
+      `<div><!-- i am comment --></div>`
+    )
+
+    // assert(
+    //   `<div>{! i am comment !}</div>`,
+    //   `<div><!-- i am comment --></div>`
+    // )
+  })
+})
