1cd16a7c838283daf91c8a847ac5330ae8a5a6d2
elcarim5efil <elcarim5efil@gmail.com>
2018-11-22 13:38:45 +0800

    chore: udpate

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const prettier = require("prettier/standalone");
+const plugins = [require("prettier/parser-html")];
+const codegen = require('./codegen');
+
+function prettify(content, options = {}) {
+  const {
+    pretty = true,
+    tabWidth = 4,
+    singleQuote = true,
+    trailingComma = 'none'
+  } = options;
+
+  return pretty ? prettier.format(content, {
+    parser: 'vue',
+    plugins,
+    tabWidth,
+    singleQuote,
+    trailingComma
+  }) : content;
+}
+
+function gen(input, options = {}) {
+  let raw = '';
+  let prettified = '';
+  let res = ''
+  try {
+    raw = codegen(input);
+    prettified = prettify(raw);
+    return raw;
+  } catch(err1) {
+    try {
+      const templateReg = /<template>([\s\S]*)<\/template>/;
+      const template = templateReg.exec(input)[1];
+      raw = codegen(template || '');
+      prettified = prettify(raw);
+      res = input.replace(template, prettified);
+    } catch(err2) {
+      console.log(err1, err2)
+    }
+  }
+  return res;
+}
+
+module.exports = gen;
